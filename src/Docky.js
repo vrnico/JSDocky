3b401080b95bad5ed8bdbf330b15490ad2ced3c3
Human Flesh Body World <hfbw@Humans-iMac.domain>
2018-03-23 14:39:55 -0700

    add api call functionality, add styling for output

--- src/Docky.js
+++ src/Docky.js
@@ -1,45 +1,43 @@
-
 import $ from 'jquery';
-export class Docky {
-  constructor(keyword){
-    this.keywordSearch = keyword;
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  }
-  //displayData calls function from frontend
-  searchKeyword(displayData, listLength){
-    const url = "https://api.betterdoctor.com/2016-03-01/doctors?query="
-    const apiKey = process.env.API_KEY;
-    let search = this.keywordSearch;
-    let suffix = "&location=or-portland&skip=0&limit=10&user_key="
-    $.ajax({
-       url: (url + search + suffix + apiKey),
-       type: 'GET',
-       data: {
-         format: 'json'
-       },
-       success: function(response) {
-         let test = [];
-         for (let i = 0; i < 100; i++){
-           if ((response.hits[i].url != null) && (response.hits[i].title != null)) {
-             test.push(response.hits[i]);
+export function Docky(keyword) {
+  $.ajax({
+    url:
+    `https://api.betterdoctor.com/2016-03-01/doctors?query=${keyword}&location=or-portland&skip=0&limit=100&user_key=${"a541d6a82003e4a2a97018e86811a19f"//process.env.exports.apiKey
+    }`,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: function(response) {
+      $('#output').append(`<h3>Docky found <span class="keywords">` + response.data.length + `</span> doctors for <div class='keywords'>${keyword}</div></h3>`);
+      if (response.data.length === 0) {
+        $('.noResult').text('The search provided no matching results.')
+      }
+      console.log(response);
+      for (let i = 0; i < response.data.length; i++) {
+        let doctor = response.data[i].profile.last_name;
+        let link = response.data[i].practices[0].website;
+        let available = response.data[i].practices[0].accepts_new_patients;
+        if(link === undefined){
+          link = "/"
+        }
+        if(available === true){
+          available = '<span class="in">AVAILABLE</span>';
+        } else {
+          available = "is NOT accepting new patients at this time";
+        }
 
-           }
-         }
-         //displayData function only works if a success
-         displayData(test, search, listLength);
-         console.log("apiKey: " + apiKey );
-         console.log("search: " + search );
-         console.log("url: " + url );
+        console.log("Dr." + doctor);
 
-       },
-       error: function() {
-       }
-    })
-  }
+        $('#output').append(`<a href='${link}'><h4>Dr.${doctor}</a>, ${available}</h4>`);
 
+      }
 
-
-
-
-
+    },
+    error: function() {
+      $('.error').text(`We are having trouble handling your request!`)
+    }
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,25 +1,14 @@
-import { Docky } from './Docky.js';
-// import { output } from './Output.js';
-import $ from 'jquery';
 import './styles.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Docky } from './Docky.js';
 
 
 $(document).ready(function() {
-  $('#keyword-search').submit(function(event) {
+  $("#userInput").submit(function(event) {
     event.preventDefault();
-
-    let newDocky = new Docky();
-
-    let displayData = function updateResults(array, keyword, results) {
-    let searchDisplay = ("#search" + keyword);
-    for (let i = 0; i < results; i++){
-      $(searchDisplay).append('<li><a href="' + array[i].url + '" target="_blank">' + array[i].title + '</a></li>');
-    }
-
-  }
-
-  console.log("page loads");
-  newDocky.searchKeyword(displayData, 10);
-})
-
+    const keyword = $('#keyword').val();
+    $('#userInput').fadeOut();
+    Docky(keyword);
+  });
 });
